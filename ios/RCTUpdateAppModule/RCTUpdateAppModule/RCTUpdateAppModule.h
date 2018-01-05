e511138b706886450fec74ade9446ba178ee9a63
gufei <gufei@learnta.com>
2018-01-05 18:58:25 +0800

    no message

---
+++ ios/RCTUpdateAppModule/RCTUpdateAppModule/RCTUpdateAppModule.h
@@ -0,0 +1,14 @@
+//
+//  RCTUpdateAppModule.h
+//  RCTUpdateAppModule
+//
+//  Created by Apple on 2018/1/5.
+//  Copyright © 2018年 Learnta Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RCTUpdateAppModule : NSObject<RCTBridgeModule>
+
+@end
